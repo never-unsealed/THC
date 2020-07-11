243522cca47e9fae14a43b18331632bbd1f90f69
1337toxic1337 <65368643+1337toxic1337@users.noreply.github.com>
2020-07-11 22:07:06 +0200

    Create Main.java

---
+++ src/toxic/Main.java
@@ -0,0 +1,134 @@
+package toxic;
+
+import logger.ConsoleLogger;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+import java.util.TimeZone;
+
+public class Main {
+
+    public static String accs = "";
+
+    public static String[] eachAcc;
+
+    public static long timeadjust = 0;
+    public static long avgRunTime = 0;
+
+    public static boolean isActive = false;
+
+
+
+    public static void main(String[] args) throws IOException, InterruptedException, ParseException {
+
+
+
+        if (args.length > 0) {
+
+            double version = 4.0;
+
+            new ProcessBuilder("cmd", "/c", "title THC v" + version).inheritIO().start().waitFor();
+            TimeZone.setDefault(TimeZone.getTimeZone("GMT"));
+
+            ConsoleLogger.fancy();
+
+            File accounts = new File("accounts.txt");
+            accounts.createNewFile();
+
+
+            Scanner accReader = new Scanner(accounts);
+            while (accReader.hasNextLine()) {
+                accs = accs + accReader.nextLine() + "\n";
+
+            }
+
+
+            if (accs.isEmpty()) {
+
+                ConsoleLogger.logError("No MC accounts found - please add accounts to accounts.txt in email:password format");
+                System.exit(1);
+
+            }
+
+            eachAcc = accs.split("\n");
+            ConsoleLogger.logInfo("Loaded " + eachAcc.length + " account(s)!");
+
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+            Date date = new Date(System.currentTimeMillis());
+            ConsoleLogger.logInfo("Current time:" + formatter.format(date) + "\n");
+
+            ConsoleLogger.logInput("Please check if your local time is accurate on https://time.is" + "\n");
+            ConsoleLogger.logInput("Do you want to adjust your time? (y/n) ");
+            BufferedReader bryt = new BufferedReader(new InputStreamReader(System.in));
+            String gotyt = bryt.readLine();
+
+            if (gotyt.equals("y")) {
+
+                ConsoleLogger.logInput("Please enter time difference in milliseconds (i.e. '-400' if you're 400ms ahead) ");
+                BufferedReader brta = new BufferedReader(new InputStreamReader(System.in));
+                String gotta = brta.readLine();
+                try {
+                    timeadjust = Long.parseLong(gotta);
+                } catch (NumberFormatException e) {
+
+                    ConsoleLogger.logError("Invalid value!");
+                    System.exit(1);
+
+                }
+
+            }
+
+            System.out.print("\n");
+            ConsoleLogger.logInput("Select mode:" + "\n");
+            ConsoleLogger.logInput("1: Manual sniper" + "\n");
+            ConsoleLogger.logInput("2: Auto mode (3-char)" + "\n");
+            ConsoleLogger.logInput("Mode: ");
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String got = br.readLine();
+
+            if (got.equals("1")) {
+
+
+
+                new InitManual();
+
+
+
+
+            } else if (got.equals("2")) {
+
+
+                new InitAuto();
+
+
+            } else {
+
+
+                ConsoleLogger.logError("Unknown action!");
+                System.exit(1);
+
+
+            }
+
+
+
+
+        } else {
+
+            String command = "cmd /c start cmd.exe /K \"@echo off && java -jar THC.jar run\"";
+            Runtime.getRuntime().exec(command);
+            System.exit(0);
+
+
+        }
+
+
+    }
+
+}
