2cc16e07cd0bd039c602dd7813764fa11b86f65b
1337toxic1337 <65368643+1337toxic1337@users.noreply.github.com>
2020-07-13 19:30:40 +0200

    Update ArrayRemove.java

--- src/snipe/ArrayRemove.java
+++ src/snipe/ArrayRemove.java
@@ -3,7 +3,7 @@ package snipe;
 import logger.ConsoleLogger;
 import toxic.Main;
 
-import java.io.Console;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -12,12 +12,23 @@ import java.util.List;
 public class ArrayRemove {
 
     private static final String[] EMPTY_STRING_ARRAY = new String[0];
-    public ArrayRemove(String email, String password){
+    public ArrayRemove(String email, String password) throws IOException {
 
         List< String > list = new ArrayList< >();
         Collections.addAll(list, Main.eachAcc);
         list.removeAll(Arrays.asList(email + ":" + password));
         Main.eachAcc = list.toArray(EMPTY_STRING_ARRAY);
+
+        String uno = Arrays.toString(Main.eachAcc).replace(", ", "\n");
+        String dos = uno.replace("[", "");
+        String tres = dos.replace(email + ":" +  password, "");
+        String cuatro = tres.replaceAll("(?m)^\\s*$[\n\r]{1,}", "");
+        File newac = new File("accounts.txt");
+        newac.createNewFile();
+
+        BufferedWriter writer1 = new BufferedWriter(new FileWriter(newac));
+        writer1.write(cuatro);
+        writer1.close();
 
         if(Main.failedLogin > 2){
 
