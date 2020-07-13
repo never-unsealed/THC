5ad01c04793b8d2a122cda19ffa52a95c6c604ba
1337toxic1337 <65368643+1337toxic1337@users.noreply.github.com>
2020-07-13 14:25:05 +0200

    Update ConsoleLogger.java

--- src/logger/ConsoleLogger.java
+++ src/logger/ConsoleLogger.java
@@ -1,38 +1,35 @@
 package logger;
 
-import org.fusesource.jansi.AnsiConsole;
-
 public class ConsoleLogger {
 
-    public static void logError(String message) {
-        AnsiConsole.out.println(RED + "[ERROR] " + WHITE + message);
-    }
+    public static void logError(String message) { System.out.println(RED + "[ERROR] " + WHITE + message); }
 
     public static void logInfo(String message) {
-        AnsiConsole.out.println(BLUE + "[INFO] " + WHITE + message );
+        System.out.println(BLUE + "[INFO] " + WHITE + message );
     }
 
     public static void logInput(String message) {
-        AnsiConsole.out.print(YELLOW + "[INPUT] " + WHITE + message);
+        System.out.print(YELLOW + "[INPUT] " + WHITE + message);
     }
 
     public static void logFailed(String message, String name) {
-        AnsiConsole.out.println(CYAN + "[" + name + "] " + RED + "[FAILED] " + WHITE + message);
+        System.out.println(CYAN + "[" + name + "] " + RED + "[FAILED] " + WHITE + message);
     }
 
     public static void logNameError(String message, String name) {
-        AnsiConsole.out.println(CYAN + "[" + name + "] " + RED + "[ERROR] " + WHITE + message);
+        System.out.println(CYAN + "[" + name + "] " + RED + "[ERROR] " + WHITE + message);
     }
 
     public static void logSuccess(String message, String name) {
-        AnsiConsole.out.println(CYAN + "[" + name + "] " + GREEN + "[SUCCESS] " + WHITE + message);
+        System.out.println(CYAN + "[" + name + "] " + GREEN + "[SUCCESS] " + WHITE + message);
     }
 
     public static void logName(String message, String name) {
-        AnsiConsole.out.println(CYAN + "[" + name + "] " + WHITE + message);
+        System.out.println(CYAN + "[" + name + "] " + WHITE + message);
     }
 
     public static void fancy() {
+
 
         String[] fancy = {" _______ _    _  _____    _____       _                 ","|__   __| |  | |/ ____|  / ____|     (_)                ","   | |  | |__| | |      | (___  _ __  _ _ __   ___ _ __ ","   | |  |  __  | |       \\___ \\| '_ \\| | '_ \\ / _ \\ '__|","   | |  | |  | | |____   ____) | | | | | |_) |  __/ |   ","   |_|  |_|  |_|\\_____| |_____/|_| |_|_| .__/ \\___|_|   ","   by smoke#1337                       | |              ","                                       |_|              "};
 
@@ -40,13 +37,12 @@ public class ConsoleLogger {
 
             for(char c:line.toCharArray()){
 
-
                 if(!(c == '_' || c == ' ' || c == '|'|| c == '/'|| c == '\\'|| c == '(' || c == ')' || c == '\'' || c == '.') ){
 
-                    AnsiConsole.out.print(GREEN + c);
+                    System.out.print(GREEN + c);
 
                 }else{
-                    AnsiConsole.out.print(PURPLE + c);
+                    System.out.print(PURPLE + c);
                 }
 
                 try {
@@ -55,19 +51,19 @@ public class ConsoleLogger {
                     e.printStackTrace();
                 }
             }
-            AnsiConsole.out.print("\n");
+            System.out.print("\n");
 
         }
-        AnsiConsole.out.print(WHITE + "\n");
+        System.out.print(WHITE + "\n");
 
     }
 
 
 
-    public static final String RED = "\u001B[31m";
+    public static final String RED = "\u001b[31;1m";
     public static final String GREEN = "\u001B[32m";
     public static final String YELLOW = "\u001B[33m";
-    public static final String BLUE = "\u001B[34m";
+    public static final String BLUE = "\u001b[34;1m";
     public static final String CYAN = "\u001B[36m";
     public static final String WHITE = "\u001B[37m";
     public static final String PURPLE = "\u001B[35m";
